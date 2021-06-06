ee10e944688df333ca819a5a50e7d0a80d8ae640
TusharGoel <tgoel219@gmail.com>
2021-06-06 17:30:17 +0530

    Server Connected with Voice Component

--- voice.js
+++ voice.js
@@ -1,14 +1,19 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
+const url = 'ws://localhost:8080'
+const connection = new WebSocket(url)
+connection.onopen = () =>{
+    connection.send('Voice Connected');
+}
 const recognition = new SpeechRecognition();
 recognition.continuous = true;
 recognition.interimResults = true;
 recognition.lang = 'en-US';
+let p = document.createElement('p');
 
 function runSpeechRecognition(){
     
 
-    let p = document.createElement('p');
+    
     const words = document.querySelector('.words');
     console.log(words);
     words.appendChild(p);
@@ -24,6 +29,10 @@ function runSpeechRecognition(){
 
         if (e.results[0].isFinal) {
             p.textContent = p.textContent + '\n';
+           
+            connection.send(p.textContent);
+            
+            
             words.appendChild(p);
         }
     });
@@ -36,4 +45,5 @@ function runSpeechRecognition(){
 function stopSpeechRecognition(){
     
     recognition.stop();
+    connection.send(p.textContent);
 }
